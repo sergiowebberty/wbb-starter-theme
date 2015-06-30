d2d82ac19cdf32416847412b6a1ee094d5003ded
adrian-webberty <softmixt@gmail.com>
2015-06-30 16:13:40 +0200

    removing vendor include on general.css, adding empty Neat_Menu_walker class

--- config/theme-lib-includes.php
+++ config/theme-lib-includes.php
@@ -3,3 +3,5 @@
 	header ( 'HTTP/1.0 404 Not Found' , TRUE , 404 );
 	die( "404 Not Found" );
 }
+
+require_once locate_template ( 'system/WBB-libs/WBB-System-Neat-Menu-Walker.php' );
---
+++ system/WBB-libs/WBB-System-Neat-Menu-Walker.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Neat_Walker extends Walker_Nav_Menu
+{
+
+	public function start_lvl ( &$output , $depth = 0 , $args = array () )
+	{
+
+	}
+
+	public function start_el ( &$output , $item , $depth = 0 , $args = array () , $id = 0 )
+	{
+
+	}
+
+
+	public function display_element ( $element , &$children_elements , $max_depth , $depth , $args , &$output )
+	{
+
+	}
+
+
+	public static function fallback ( $args )
+	{
+
+	}
+
+}

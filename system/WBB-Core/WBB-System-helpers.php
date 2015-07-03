c4601c83046306a09ea18d655d9455989634df44
Sergio Villegas <sergio@webberty.com>
2015-07-03 15:20:33 +0200

    breadcrumb

--- system/WBB-Core/WBB-System-helpers.php
+++ system/WBB-Core/WBB-System-helpers.php
@@ -223,7 +223,7 @@ if ( ! function_exists ( "wbb_breadcrumb" ) )
 		$category = get_the_category ();
 
 		// Build the breadcrums
-		echo '<ul id="' . $id . '" class="' . $class . '">';
+		echo '<div id="' . $id . '" class="' . $class . '">';
 
 		// Do not display on the homepage
 		if ( ! is_front_page () )
@@ -231,14 +231,14 @@ if ( ! function_exists ( "wbb_breadcrumb" ) )
 
 			// Home page
 			echo '<li class="item-home"><a class="bread-link bread-home"  itemprop="url" href="' . get_home_url () . '" title="' . $home_title . '">' . $home_title . '</a></li>';
-			echo '<li class="separator separator-home"> ' . $separator . ' </li>';
+			//echo '<li class="separator separator-home"> ' . $separator . ' </li>';
 
 			if ( is_single () )
 			{
 
 				// Single post (Only display the first category)
 				echo '<li class="item-cat item-cat-' . $category[ 0 ]->term_id . ' item-cat-' . $category[ 0 ]->category_nicename . '"><a  itemprop="url" class="bread-cat bread-cat-' . $category[ 0 ]->term_id . ' bread-cat-' . $category[ 0 ]->category_nicename . '" href="' . get_category_link ( $category[ 0 ]->term_id ) . '" title="' . $category[ 0 ]->cat_name . '">' . $category[ 0 ]->cat_name . '</a></li>';
-				echo '<li class="separator separator-' . $category[ 0 ]->term_id . '"> ' . $separator . ' </li>';
+				//echo '<li class="separator separator-' . $category[ 0 ]->term_id . '"> ' . $separator . ' </li>';
 				echo '<li class="item-current item-' . $post->ID . '"><strong class="bread-current bread-' . $post->ID . '" title="' . get_the_title () . '">' . get_the_title () . '</strong></li>';
 
 			}
@@ -266,7 +266,7 @@ if ( ! function_exists ( "wbb_breadcrumb" ) )
 					foreach ( $anc as $ancestor )
 					{
 						$parents .= '<li class="item-parent item-parent-' . $ancestor . '"><a  itemprop="url" class="bread-parent bread-parent-' . $ancestor . '" href="' . get_permalink ( $ancestor ) . '" title="' . get_the_title ( $ancestor ) . '">' . get_the_title ( $ancestor ) . '</a></li>';
-						$parents .= '<li class="separator separator-' . $ancestor . '"> ' . $separator . ' </li>';
+						//$parents .= '<li class="separator separator-' . $ancestor . '"> ' . $separator . ' </li>';
 					}
 
 					// Display parent pages
@@ -307,11 +307,11 @@ if ( ! function_exists ( "wbb_breadcrumb" ) )
 
 				// Year link
 				echo '<li class="item-year item-year-' . get_the_time ( 'Y' ) . '"><a  itemprop="url" class="bread-year bread-year-' . get_the_time ( 'Y' ) . '" href="' . get_year_link ( get_the_time ( 'Y' ) ) . '" title="' . get_the_time ( 'Y' ) . '">' . get_the_time ( 'Y' ) . ' Archives</a></li>';
-				echo '<li class="separator separator-' . get_the_time ( 'Y' ) . '"> ' . $separator . ' </li>';
+				//echo '<li class="separator separator-' . get_the_time ( 'Y' ) . '"> ' . $separator . ' </li>';
 
 				// Month link
 				echo '<li class="item-month item-month-' . get_the_time ( 'm' ) . '"><a  itemprop="url" class="bread-month bread-month-' . get_the_time ( 'm' ) . '" href="' . get_month_link ( get_the_time ( 'Y' ) , get_the_time ( 'm' ) ) . '" title="' . get_the_time ( 'M' ) . '">' . get_the_time ( 'M' ) . ' Archives</a></li>';
-				echo '<li class="separator separator-' . get_the_time ( 'm' ) . '"> ' . $separator . ' </li>';
+				//echo '<li class="separator separator-' . get_the_time ( 'm' ) . '"> ' . $separator . ' </li>';
 
 				// Day display
 				echo '<li class="item-current item-' . get_the_time ( 'j' ) . '"><strong class="bread-current bread-' . get_the_time ( 'j' ) . '"> ' . get_the_time ( 'jS' ) . ' ' . get_the_time ( 'M' ) . ' Archives</strong></li>';
@@ -324,7 +324,7 @@ if ( ! function_exists ( "wbb_breadcrumb" ) )
 
 				// Year link
 				echo '<li class="item-year item-year-' . get_the_time ( 'Y' ) . '"><a  itemprop="url" class="bread-year bread-year-' . get_the_time ( 'Y' ) . '" href="' . get_year_link ( get_the_time ( 'Y' ) ) . '" title="' . get_the_time ( 'Y' ) . '">' . get_the_time ( 'Y' ) . ' Archives</a></li>';
-				echo '<li class="separator separator-' . get_the_time ( 'Y' ) . '"> ' . $separator . ' </li>';
+				//echo '<li class="separator separator-' . get_the_time ( 'Y' ) . '"> ' . $separator . ' </li>';
 
 				// Month display
 				echo '<li class="item-month item-month-' . get_the_time ( 'm' ) . '"><strong class="bread-month bread-month-' . get_the_time ( 'm' ) . '" title="' . get_the_time ( 'M' ) . '">' . get_the_time ( 'M' ) . ' Archives</strong></li>';
@@ -373,7 +373,7 @@ if ( ! function_exists ( "wbb_breadcrumb" ) )
 
 		}
 
-		echo '</ul>';
+		echo '</div>';
 	}
 }
 

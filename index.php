c214a7b9cbb1bf3054d801c717d8e76fd4c31c3d
adrian-webberty <softmixt@gmail.com>
2015-07-09 12:47:50 +0200

    remove unnecessary query_post function

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
 	<?php get_template_part ( 'templates/content' , get_post_type () != 'post' ? get_post_type () : get_post_format () ); ?>
 
-<?php endwhile; wp_reset_query(); ?>
+<?php endwhile; ?>
 
 
 <?php echo wbb_custom_pagination( ); ?>
